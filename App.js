42c6f9eb14ae2b9b9d693ea9e6c9c67720c78da1
tiredpanda254 <116378094+tiredpanda254@users.noreply.github.com>
2022-11-13 16:23:19 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div className="App">
+      <button onClick={() => setCount(count + 1)}>Click me!</button>
+			<h2>Current count: {count}</h2>
+    </div>
+  );
+}
+
+export default App;
